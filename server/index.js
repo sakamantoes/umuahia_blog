d80189b2c973ef18b6c123b0cea80527a671118c
leoTechHub <godswilltrade@gmail.com>
2026-03-01 00:44:27 +0100

    admin create details

--- server/index.js
+++ server/index.js
@@ -3,8 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
-import bcrypt from 'bcryptjs';
-import User from './models/User.js';
+import createDefaultAdmin from './middleware/createDefaultAdmin.js';
 import { fileURLToPath } from 'url';
 
 import UserRoutes from './routes/auth.js';
@@ -24,42 +23,17 @@ app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Database connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://godswilltrade2004_db_user:q3WG7VecnYGwE335@cluster0.jcc8ghr.mongodb.net/?appName=Cluster0')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://godswilltrade2004_db_user:q3WG7VecnYGwE335@cluster0.jcc8ghr.mongodb.net/?appName=Cluster0')
+  .then(async () => {
+    console.log('Connected to MongoDB');
 
-  // After mongoose.connect
-const createDefaultAdmin = async () => {
-  try {
-    const adminExists = await User.findOne({ role: 'admin' });
-    
-    if (!adminExists) {
-      const hashedPassword = await bcrypt.hash('Admin@123', 10);
-      const admin = new User({
-        fullName: 'System Administrator',
-        email: 'admin@umuahiasouth.gov.ng',
-        phone: '08012345678',
-        dateOfBirth: new Date('1990-01-01'),
-        community: 'Umuahia Urban',
-        address: 'Umuahia South LGA Secretariat',
-        occupation: 'Administrator',
-        password: hashedPassword,
-        role: 'admin'
-      });
-      
-      await admin.save();
-      console.log('Default admin created:');
-      console.log('Email: admin@umuahiasouth.gov.ng');
-      console.log( hashedPassword); //Admin@123 is the password 
-    }
-  } catch (error) {
-    console.error('Error creating default admin:', error);
-  }
-};
+    // Call AFTER connection
+    await createDefaultAdmin();
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
 
-// Call this after database connection
-createDefaultAdmin();
 
   app.use('/api/users', UserRoutes);
   app.use('/api/posts', PostRoutes);
